d25afe06930652149248f3cb818d6ce15c6cbf17
Apple <trinq110902@gmail.com>
2023-07-06 14:22:50 +0700

    update checkbox

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
 import { ChakraProvider, ColorModeScript, theme } from "@chakra-ui/react"
+import Search from "../components/elements/search/Search"
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
     return (
         <ChakraProvider theme={theme}>
             <ColorModeScript />
             <Component {...pageProps} />
+            <Search />
         </ChakraProvider>
     )
 }
